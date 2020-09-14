c47b0fd864d54b73c767acd73c6a398078bd9354
Naveen <mailtomassnaveen@gmail.com>
2020-09-14 22:29:54 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
 import "./styles.css";
-import Tabs from "./tab";
-import TabPane from "./tab-pane";
 
 export default function App() {
   return (
     <div className="App">
       <div className="header">React Employee Hierarchy Chart</div>
-      <div className="container">
-        <Tabs>
-          <TabPane name="Tab 1" key="1">
-            Content of Tab Pane 1
-          </TabPane>
-          <TabPane name="Tab 2" key="2">
-            Content of Tab Pane 2
-          </TabPane>
-          <TabPane name="Tab 3" key="3">
-            Content of Tab Pane 3
-          </TabPane>
-        </Tabs>
-      </div>
+      <div className="container"></div>
       <footer>
         <a href="https://twitter.com/_naveenda">
           <img
